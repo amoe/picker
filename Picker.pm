3183dedd42315dd2d64ef717367b17b8f7b83794
David Banks <amoebae@gmail.com>
2017-01-22 11:55:25 +0000

    reading file tree

--- Picker.pm
+++ Picker.pm
@@ -5,6 +5,8 @@ use warnings;
 use v5.20.2;
 use autodie qw(:all);
 use String::ShellQuote qw/shell_quote/;
+use Data::Dump qw/dump/;
+use File::Find;
 
 
 sub get_audio_file_length {
@@ -31,6 +33,29 @@ sub get_audio_file_album {
     chomp $output;
 
     return $output;
+}
+
+# $_ is set to the file here.
+sub make_wanted_function {
+    my $target_hash_reference = shift;
+
+    return sub {
+        if (-f) {
+            my $album_name =  get_audio_file_album($File::Find::name);
+            $target_hash_reference->{$album_name} = 42;
+        }
+    }
+}
+
+sub scan_dir {
+    my $root = shift;
+    my %output = ();
+
+    find(make_wanted_function(\%output), $root);
+
+    say dump(\%output);
+
+    return \%output;
 }
 
 1;
